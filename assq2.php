33c9958673e9819f222598bfae4256410a348e53
Niyigena18 <111857107+Niyigena18@users.noreply.github.com>
2022-09-16 11:31:42 +0200

    Add files via upload

---
+++ assq2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>question one</title>
+</head>
+<body>
+<?php
+    function sum(int $w,int $x,int $y,int $z){
+        return $w+$x+$y+$z;
+    }
+echo sum(10,10,10,10)
+?>
+</body>
+</html>
---
+++ assq3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>question 3</title>
+</head>
+<body>
+
+</body>
+</html>
+    <?php
+    $name ='orah';
+    $present ='watch';
+    $age=21;
+    $aged=20;
+    $city='nyanza';
+    $yourName='niyigena irene';
+
+     echo'Dear ';
+     echo $name;
+     echo', <br>';
+     echo'Thank you for the ';
+     echo $present;
+     echo'. I really like it. <br>';
+     echo'I can’t believe I’m aleady ';
+     echo $age ;
+     echo' years old, but it doesn’t fell that much different than being  <br>';
+     echo $aged;
+     echo'. <br>';
+     echo'I hope that some time in the next year we can come visit you in ';
+     echo  $city;
+     echo'.<br>';
+     echo'Thank you once again for the ';
+     echo $present;
+     echo'. <br>';
